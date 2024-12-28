ec17bf1f891e4e802130a628d9ac69607f9cf848
0ankit0 <ankitpaudel.1373@gmail.com>
2024-12-28 15:52:20 +0545

    Add custom auth policy configuration method
    A new `using` directive for `Microsoft.Extensions.DependencyInjection` has been added.
    Introduced a new namespace `Authconfig.Configuration`.
    Added a static class `AuthPolicy` within the new namespace.
    Implemented a static method `AddCustomAuthPolicy` in `AuthPolicy`.
    The method configures an authentication policy using external configuration.
    It takes an `IServiceCollection` instance, a policy name, and an array of required roles as parameters.
    The method adds a policy requiring an authenticated user with specified roles.
    Usage example: `builder.Services.AddCustomAuthenticationPolicy("AdminOrManagerPolicy", new[] { "Admin", "Manager" });`.

---
+++ Authconfig/Configuration/AuthPolicy.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Authconfig.Configuration
+{
+    public static class AuthPolicy
+    {
+        // This method will configure the authentication policy using external configuration
+        //to use this simply add:
+        //builder.Services.AddCustomAuthenticationPolicy("AdminOrManagerPolicy", new[] { "Admin", "Manager" });
+
+        public static void AddCustomAuthPolicy(this IServiceCollection services, string policyName, string[] requiredRoles)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(policyName, policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    foreach (var role in requiredRoles)
+                    {
+                        policy.RequireRole(role); // Require all roles passed from the main project
+                    }
+                });
+            });
+        }
+    }
+}
